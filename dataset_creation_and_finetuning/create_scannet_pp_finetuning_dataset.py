509aabef90e2f123a1927178ea8cd749d9ae7a7c
Joao <jmc12@illinois.edu>
2025-02-23 01:32:38 -0600

    new training code

---
+++ dataset_creation_and_finetuning/create_scannet_pp_finetuning_dataset.py
@@ -0,0 +1,98 @@
+# for param in model.parameters():
+#     param.requires_grad = False
+# decoder_head = model.decode_head
+# decoder_head.classifier = nn.Conv2d(768,21,kernel_size=(1, 1), stride=(1, 1))
+# for param in decoder_head.parameters():
+#     param.requires_grad = True
+
+from glob import glob
+import pandas as pd 
+import cv2
+from joblib import Parallel,delayed
+from datasets import Dataset
+import numpy as np
+import pickle
+from datasets import IterableDataset
+from tqdm import tqdm
+
+# frames = pd.Series(sorted(glob('/home/motion/data/scannet_pp/data/**/gt_semantics/*.png',recursive = True)))
+# frame_names = frames.str.split('/',expand = True).iloc[:,-1].str.split('.',expand = True).iloc[:,0]
+# rgb_frames = frames.str.rsplit('/',expand = True,n = 2).iloc[:,0] + '/rgb/'+frame_names+'.jpg'
+
+# img1 = '/home/motion/data/scannet_pp/data/1a8e0d78c0/iphone/gt_semantics/frame_000030.png'
+# tmp = cv2.imread(frames[0],cv2.IMREAD_UNCHANGED)
+# tmp2 = cv2.imread(rgb_frames[0])
+# plt.figure(1)
+# plt.imshow(tmp)
+# plt.figure(2)
+# plt.imshow(tmp2)
+# plt.show()
+
+
+
+train_scenes = pd.read_csv('./newtrain.txt', header = None)
+val_scenes = pd.read_csv('./newval.txt',header = None)
+test_scenes =  pd.read_csv('./newtest.txt',header = None)
+
+# def data_generator():
+i = 0
+sfs = []
+rgbs = []
+all_frames = pd.Series(sorted(glob('/home/motion/data/scannet_pp/data/**/gt_semantics/*.png',recursive = True)))
+scene_names = all_frames.str.split('/',expand = True).iloc[:,-4]
+train_semantic_images = all_frames.loc[scene_names.isin(train_scenes.iloc[:,0])]
+val_semantic_images = all_frames.loc[scene_names.isin(val_scenes.iloc[:,0])]
+
+
+
+def verify_all_data_and_create_dataset(semantic_frames):
+    rgbs = []
+    sfs = []
+    frame_names = semantic_frames.str.split('/',expand = True).iloc[:,-1].str.split('.',expand = True).iloc[:,0]
+    rgb_frames = semantic_frames.str.rsplit('/',expand = True,n = 2).iloc[:,0] + '/rgb/'+frame_names+'.jpg'
+    n = rgb_frames.shape[0]
+#     for semantic_frame,rgb_frame in tqdm(zip(semantic_frames.values,rgb_frames.values)):
+#         try:
+#             tmp = cv2.imread(semantic_frame,cv2.IMREAD_UNCHANGED)
+#             tmp2 = cv2.imread(rgb_frame)
+#             if((tmp is not None) and (tmp2 is not None)):
+#                 rgbs.append(rgb_frame)
+#                 sfs.append(semantic_frame)
+#         except Exception as e:
+#             i+=1
+#             print(e)
+#             continue
+    all_verified_frames = Parallel(n_jobs = 6,verbose = 9,backend = 'threading')(delayed(verify_this_frame)(image_dirs)for image_dirs in zip(semantic_frames.values,rgb_frames.values))
+    all_verified_frames = np.array(all_verified_frames)
+    invalid = all_verified_frames =='none'
+    invalid_entries = np.any(invalid,axis = 1)
+    valid_entries = all_verified_frames[np.logical_not(invalid_entries)]
+    rgbs = valid_entries[:,0]
+    sfs = valid_entries[:,1]
+    
+    return {'pixel_values':rgbs,'label':sfs}
+
+def verify_this_frame(image_dirs):
+    semantic_frame,rgb_frame = image_dirs
+    try:
+        tmp = cv2.imread(semantic_frame,cv2.IMREAD_UNCHANGED)
+        tmp2 = cv2.imread(rgb_frame)
+        if((tmp is not None) and (tmp2 is not None)):
+            return [rgb_frame,semantic_frame]
+        else:
+            return ['none','none']
+        
+    except Exception as e:
+        i+=1
+        print(e)
+        return ['none','none']
+        
+# train_dict = verify_all_data_and_create_dataset(train_semantic_images)
+# pickle.dump(train_dict,open('./scannet_pp_train_dict.p','wb'))
+# train_ds = Dataset.from_dict(train_dict)
+# train_ds.save_to_disk('./scannet_pp_finetune_train.hf')
+
+val_dict = verify_all_data_and_create_dataset(val_semantic_images)
+pickle.dump(val_dict,open('./scannet_pp_val_dict.p','wb'))
+val_ds = Dataset.from_dict(val_dict)
+val_ds.save_to_disk('./scannet_pp_finetune_val.hf')
--- dataset_creation_and_finetuning/finetune_segformer.py
+++ dataset_creation_and_finetuning/finetune_segformer.py
@@ -12,62 +12,106 @@ from transformers import (
     Trainer,
     TrainingArguments,
 )
-
-
+import cv2
+import pickle
+import pdb
 import sys,os
+import time
+
+from datasets import IterableDataset,Dataset
+
+# def data_generator():
+#     i = 0
+#     semantic_frames = pd.Series(sorted(glob('/home/motion/data/scannet_pp/data/**/*.png',recursive = True)))
+#     frame_names = semantic_frames.str.split('/',expand = True).iloc[:,-1].str.split('.',expand = True).iloc[:,0]
+#     rgb_frames = semantic_frames.str.rsplit('/',expand = True,n = 2).iloc[:,0] + '/rgb/'+frame_names+'.jpg'
+#     n = rgb_frames.shape[0]
+#     while(i<n):
+#         try:
+#             tmp = cv2.imread(semantic_frames[i],cv2.IMREAD_UNCHANGED)
+#             tmp2 = cv2.imread(rgb_frames[i])
+#             i +=1
+#             yield {'rgb':tmp2,'gt':tmp}
+#         except Exception as e:
+#             i+=1
+#             continue
+N_CLASSES = 101
+
+# ds = IterableDataset.from_generator(data_generator)
+
+
+def load_images(rgb,semantic,):
+    # import pdb
+    # pdb.set_trace()
+    tmp = cv2.imread(semantic[0],cv2.IMREAD_UNCHANGED).astype(np.uint8)
+    tmp2 = cv2.imread(rgb[0]).astype(np.uint8)
+    tmp2 = np.moveaxis(tmp2,[0,1,2],[1,2,0])
+    return tmp2,tmp
+
+
 parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir)
 
-from utils.ScanNet_scene_definitions import get_filenames
 
-fnames = get_filenames()
+# fnames = get_filenames()
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 model =  SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b4-finetuned-ade-512-512").to(device)
-huggingface_dataset_dir = fnames["ScanNet_huggingface_dataset_dir"]
 
-COLORS = np.array([
-    [0,0,0],[151,226,173],[174,198,232],[31,120,180],[255,188,120],[188,189,35],[140,86,74],[255,152,151],[213,39,40],[196,176,213],[148,103,188],[196,156,148],
-    [23,190,208],[247,183,210],[218,219,141],[254,127,14],[227,119,194],[158,218,229],[43,160,45],[112,128,144],[82,83,163]
-]).astype(np.uint8)
 
 model.train()
 for param in model.parameters():
     param.requires_grad = False
 decoder_head = model.decode_head
-decoder_head.classifier = nn.Conv2d(768,21,kernel_size=(1, 1), stride=(1, 1))
+decoder_head.classifier = nn.Conv2d(768,N_CLASSES,kernel_size=(1, 1), stride=(1, 1))
 for param in decoder_head.parameters():
     param.requires_grad = True
 
-classes = ['irrelevant','wall','floor','cabinet','bed','chair','sofa','table','door','window','bookshelf','picture',
-           'counter','desk','curtain','refridgerator','shower curtain','toilet','sink','bathtub','otherfurniture']
-id2label = {}
-label2id = {}
-print("{")
-for i in range(len(classes)):
-    comma = ''
-    if(i!= len(classes)-1):
-        comma = ','
-    print('\"{}\":\"{}\"{}'.format(classes[i],i,comma))
-    id2label.update({i:classes[i]})
-print('}')
+model.to(device)
+# classes = ['irrelevant','wall','floor','cabinet','bed','chair','sofa','table','door','window','bookshelf','picture',
+#            'counter','desk','curtain','refridgerator','shower curtain','toilet','sink','bathtub','otherfurniture']
+# id2label = {}
+# label2id = {}
+# print("{")
+# for i in range(len(classes)):
+#     comma = ''
+#     if(i!= len(classes)-1):
+#         comma = ','
+#     print('\"{}\":\"{}\"{}'.format(classes[i],i,comma))
+#     id2label.update({i:classes[i]})
+# print('}')
 
 feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/segformer-b4-finetuned-ade-512-512")
 feature_extractor.reduce_labels = False
 
-ds = Dataset.load_from_disk(huggingface_dataset_dir)
-ds = ds
-ds = ds.train_test_split(test_size=0.2)
-train_ds = ds["train"]
-full_test_ds = ds["test"]
-test_ds = full_test_ds.shuffle(seed = 1).train_test_split(test_size = 0.1)['test']
+train_ds_dir = './scannet_pp_finetune_train.hf'
+val_ds_dir = './scannet_pp_finetune_val.hf'
+
+train_ds = Dataset.load_from_disk(train_ds_dir)
+val_ds = Dataset.load_from_disk(val_ds_dir)
+
+# ds = Dataset.load_from_disk(huggingface_dataset_dir,keep_in_memory = True)
+# ds = ds
+# ds = ds.train_test_split(test_size=0.2)
+# train_ds = ds["train"]
+
+
+# full_test_ds = ds["test"]
+# test_ds = full_test_ds.shuffle(seed = 1).train_test_split(test_size = 0.1)['test']
 
 jitter = ColorJitter(brightness=0.25, contrast=0.25, saturation=0.25, hue=0.1) 
 
 def train_transforms(example_batch):
 #     images = [x for x in example_batch['pixel_values']]
 #     labels = [x for x in example_batch['label']]
-    inputs = feature_extractor(example_batch['pixel_values'], example_batch['label'])
-    
+    # import pdb
+    # pdb.set_trace()
+
+    # a = np.array(example_batch['pixel_values']).astype(np.uint8)[0]
+    # b = np.array(example_batch['label']).astype(np.uint8)[0]
+    a,b = load_images(example_batch['pixel_values'],example_batch['label'])
+
+    inputs = feature_extractor(a,b)
+    # pdb.set_trace()
     return inputs
 
 
@@ -75,27 +119,69 @@ def val_transforms(example_batch):
 #     images = [x for x in example_batch['pixel_values']]
 #     labels = [x for x in example_batch['label']]
 #     inputs = feature_extractor(images, labels)
-    inputs = feature_extractor(example_batch['pixel_values'], example_batch['label'])
+    a,b = load_images(example_batch['pixel_values'],example_batch['label'])
+    inputs = feature_extractor(a,b)
     
     return inputs
 
 # Set transforms
 train_ds.set_transform(train_transforms)
-test_ds.set_transform(val_transforms)
+val_ds.set_transform(val_transforms)
 
 # inverse class frequency error weighing
-weights = np.array(([ 0.86863202,  1.        ,  1.26482577,  4.97661045,  6.21128435,
-        4.0068586 ,  8.72477767,  4.93037224,  5.65326448, 16.44580194,
-       18.8649601 , 55.24242013, 29.60985561, 11.04643569, 20.82360894,
-       30.38149462, 45.75461865, 32.74486949, 50.70553433, 30.23161118,
-        7.48407616])).astype(np.float32)
+# weights = np.array(([ 0.86863202,  1.        ,  1.26482577,  4.97661045,  6.21128435,
+#         4.0068586 ,  8.72477767,  4.93037224,  5.65326448, 16.44580194,
+#        18.8649601 , 55.24242013, 29.60985561, 11.04643569, 20.82360894,
+#        30.38149462, 45.75461865, 32.74486949, 50.70553433, 30.23161118,
+#         7.48407616])).astype(np.float32)
+
+# weights = np.ones(151)
+# weights = np.array([3.57321040e+00, 0.00000000e+00, 0.00000000e+00, 7.08178666e+00,
+#        0.00000000e+00, 4.14079262e+01, 0.00000000e+00, 2.05134159e+04,
+#        5.27260114e+01, 0.00000000e+00, 2.07407545e+01, 0.00000000e+00,
+#        0.00000000e+00, 0.00000000e+00, 4.02161544e+01, 1.14332567e+01,
+#        0.00000000e+00, 3.52378877e+02, 2.70503919e+02, 2.02602728e+01,
+#        0.00000000e+00, 0.00000000e+00, 6.34202531e+02, 1.95732787e+02,
+#        1.67273495e+02, 0.00000000e+00, 0.00000000e+00, 4.64582446e+03,
+#        8.40246160e+03, 0.00000000e+00, 1.34579615e+03, 6.83789516e+03,
+#        0.00000000e+00, 1.43370158e+02, 0.00000000e+00, 1.06374526e+03,
+#        3.27712097e+02, 0.00000000e+00, 4.72086901e+05, 2.20186432e+03,
+#        0.00000000e+00, 8.57574874e+01, 5.25179543e+02, 0.00000000e+00,
+#        6.46439060e+05, 6.31165003e+02, 0.00000000e+00, 1.17806968e+03,
+#        0.00000000e+00, 0.00000000e+00, 9.88847656e+02, 0.00000000e+00,
+#        0.00000000e+00, 1.14776052e+03, 0.00000000e+00, 2.04510285e+03,
+#        0.00000000e+00, 8.25563342e+03, 0.00000000e+00, 0.00000000e+00,
+#        0.00000000e+00, 0.00000000e+00, 3.74999491e+01, 7.04992439e+01,
+#        0.00000000e+00, 1.02464345e+04, 3.56803158e+04, 2.81996793e+02,
+#        0.00000000e+00, 6.26742915e+02, 3.43953330e+04, 6.08058623e+03,
+#        0.00000000e+00, 2.37217990e+02, 1.19177611e+02, 0.00000000e+00,
+#        0.00000000e+00, 2.15204603e+05, 0.00000000e+00, 0.00000000e+00,
+#        0.00000000e+00, 6.61436138e+03, 1.23352336e+03, 0.00000000e+00,
+#        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+#        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+#        4.19741084e+02, 0.00000000e+00, 0.00000000e+00, 3.54751507e+04,
+#        0.00000000e+00, 5.43585072e+04, 1.16217891e+03, 0.00000000e+00,
+#        4.23087875e+02, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+#        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 6.16356597e+03,
+#        0.00000000e+00, 0.00000000e+00, 6.23274815e+02, 1.96132662e+04,
+#        3.96258422e+03, 0.00000000e+00, 0.00000000e+00, 4.12327557e+02,
+#        0.00000000e+00, 0.00000000e+00, 5.83507259e+03, 1.90547152e+04,
+#        6.72617183e+04, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+#        3.21199779e+03, 1.82823569e+03, 0.00000000e+00, 1.83371163e+04,
+#        0.00000000e+00, 1.93259397e+03, 4.75639498e+02, 6.65932929e+03,
+#        0.00000000e+00, 5.23085590e+02, 0.00000000e+00, 8.23645939e+03,
+#        0.00000000e+00, 1.72973788e+03, 4.13863435e+02, 8.00092090e+02,
+#        0.00000000e+00, 0.00000000e+00, 7.95932510e+03, 2.80106544e+01,
+#        2.58610958e+01, 0.00000000e+00, 1.64592010e+02, 4.26831910e+03,
+#        1.67464229e+04, 0.00000000e+00, 8.73879480e+00]).astype(np.float32)
+weights = np.ones(101).astype(np.float32)
 
 
-epochs = 10
-lr = 0.01
-batch_size = 12
+epochs = 500
+lr = 0.001
+batch_size = 200
 
-hub_model_id = "finetuned ScanNet2"
+hub_model_id = "finetuned ScanNetpp"
 
 training_args = TrainingArguments(
     "ScanNet Finetuned SegFormer",
@@ -106,20 +192,28 @@ training_args = TrainingArguments(
     save_total_limit=3,
     evaluation_strategy="steps",
     save_strategy="steps",
-    save_steps=400,
-    eval_steps=400,
+    save_steps=200,
+    eval_steps=200,
     logging_steps=1,
     eval_accumulation_steps=1,
     gradient_accumulation_steps = 3,
     load_best_model_at_end=True,
-    metric_for_best_model='mean_iou'
+    metric_for_best_model='mean_iou',
+    dataloader_num_workers = 8,
+    fp16 = True
+    # lr_scheduler_type = 'reduce_lr_on_plateau',
 )
 
 metric = evaluate.load("mean_iou")
 
 def compute_metrics(eval_pred):
+
+    start = time.time()
     with torch.no_grad():
+        
         logits, labels = eval_pred
+        labels[labels == 255] = N_CLASSES-1
+
         logits_tensor = torch.from_numpy(logits)
 #         print(logits_tensor.size())
         # scale the logits to the size of the label
@@ -131,13 +225,14 @@ def compute_metrics(eval_pred):
         ).argmax(dim=1)
 
         pred_labels = logits_tensor.detach().cpu().numpy()
+        label = labels
         # currently using _compute instead of compute
         # see this issue for more info: https://github.com/huggingface/evaluate/pull/328#issuecomment-1286866576
         metrics = metric._compute(
                 predictions=pred_labels,
                 references=labels,
-                num_labels=len(id2label),
-                ignore_index=0,
+                num_labels=N_CLASSES,
+                ignore_index=N_CLASSES-1,
                 reduce_labels=feature_extractor.reduce_labels,
             )
 
@@ -145,17 +240,18 @@ def compute_metrics(eval_pred):
         per_category_accuracy = metrics.pop("per_category_accuracy").tolist()
         per_category_iou = metrics.pop("per_category_iou").tolist()
 
-        metrics.update({f"accuracy_{id2label[i]}": v for i, v in enumerate(per_category_accuracy)})
-        metrics.update({f"iou_{id2label[i]}": v for i, v in enumerate(per_category_iou)})
-
+        metrics.update({f"accuracy_{i}": v for i, v in enumerate(per_category_accuracy)})
+        metrics.update({f"iou_{i}": v for i, v in enumerate(per_category_iou)})
+        print('metric calculations took {}'.format(time.time()-start))
         return metrics
 
-early_stop = EarlyStoppingCallback(5,0.0005)
+early_stop = EarlyStoppingCallback(20,0.0005)
 
 
 class CustomTrainer(Trainer):
-    def compute_loss(self, model, inputs, return_outputs=False):
+    def compute_loss(self, model, inputs, return_outputs=False,num_items_in_batch = 10):
         labels = inputs.get("labels").to(model.device)
+        labels[labels == 255] = N_CLASSES-1
         # forward pass
         outputs = model(**inputs)
         logits = outputs.get('logits').to(model.device)
@@ -166,7 +262,10 @@ class CustomTrainer(Trainer):
             align_corners=False,
         )
         # compute custom loss
+        # pdb.set_trace()
+
         loss_fct = nn.CrossEntropyLoss(weight=torch.tensor(weights).to(model.device))
+        # pdb.set_trace()
         loss = loss_fct(logits, labels)
         return (loss, outputs) if return_outputs else loss
     
@@ -174,7 +273,7 @@ trainer = CustomTrainer(
     model=model,
     args=training_args,
     train_dataset=train_ds,
-    eval_dataset=test_ds,
+    eval_dataset=val_ds,
     compute_metrics=compute_metrics,
     callbacks=[early_stop]
 )
